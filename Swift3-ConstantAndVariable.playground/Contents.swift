34a81d2b8315f52911135a72d6a306f9bb8e4dac
Sridhar Sanapala <sridharcse@gmail.com>
2016-10-23 16:43:44 -0500

    Added Constant and Variable in play ground
    Created play ground file to explain constants and variables creation in
    Swift 3.

---
+++ Swift3-ConstantAndVariable.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let nameOfProgrammingLanguage = "Swift"
+
+var numberOfYears = 8
+
+//Uncommenting the below line results in an error, as we're trying to update the value to a constant
+//nameOfProgrammingLanguage = "Swift 3"
+
+numberOfYears = 11
+
+
